0a1079dc2b5231069b68b9a4ffa20a9ab4f94722
zahidalidev <m.zahidalidev@gmail.com>
2021-02-12 20:40:06 +0500

    cors commented

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from flask_cors import CORS
+# from flask_cors import CORS
 
 from resources.ocr import OCR
 from resources.routeTest import RouteTest
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 api = Api(app)
 
 api.add_resource(OCR, '/ocr', '/ocr/<string:id>')
